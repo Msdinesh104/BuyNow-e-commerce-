1d664f3511ce1cb031128906e3840950bb111dda
Msdinesh104 <dinesh.ms0003@gmail.com>
2026-01-11 12:15:22 +0530

    name change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
       {/* HERO SECTION */}
       <section className="max-w-7xl mx-auto px-4 py-20 text-center">
         <h1 className="text-4xl md:text-6xl font-bold text-blue-800 mb-6">
-          Welcome to <span className="text-blue-700">Bazario</span>
+          Welcome to <span className="text-blue-700">BuyNow</span>
         </h1>
 
         <p className="text-gray-600 text-lg md:text-xl max-w-3xl mx-auto mb-10">
